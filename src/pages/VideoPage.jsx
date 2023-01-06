9cce6a2947f7dd6a045019fe3b63dbd527ddc3ff
vitouaj <vitouaj68@gmail.com>
2023-01-06 15:48:42 +0700

    style parnter logos

--- src/pages/VideoPage.jsx
+++ src/pages/VideoPage.jsx
@@ -13,38 +13,46 @@ export default function VideoPage(props) {
     "success! your free report will arrive in 10 mins! while you wait watch this...";
   const title = "Discover how e-invoicing work";
 
-  const partnerLogo = [camtech, ideathon, nicc, techStartup, twd];
+  const partnerLogo = [camtech, techStartup, nicc, twd, ideathon];
   return (
-    <div className="grid justify-items-center">
+    <div className="grid justify-items-center mx-20 mt-4">
       <div>
         <img className="w-56" src={PolymerImage}></img>
       </div>
 
-      <p className="bg-sky-200 w-fit px-6 rounded-2xl mt-14 mb-4">
+      <p className="bg-sky-200 w-fit py-1 px-10 rounded-2xl mt-14 mb-4">
         <img className="inline-block w-4 align-top mt-1" src={TickMark} />
-        <span className="text-blue-500 font-semibold">
+        <span className="text-blue-500 text-sm font-semibold">
           {text.toUpperCase()}
         </span>
       </p>
 
       <h1 className="text-3xl font-bold">"{title.toUpperCase()}"</h1>
 
-      <div className="mt-6 mb-4">
+      <div className="mt-6 w-3/4 mb-4">
         <video className="w-full" controls src=""></video>
       </div>
 
-      <button className="mb-2 w-full pl-11 rounded pr-4 cursor-pointer bg-blue-500 text-white border border-gray-300 rounded-sm py-2 hover:bg-sky-400">
+      <button className="w-1/2 mb-10 w-full pl-11 rounded pr-4 cursor-pointer bg-blue-500 text-white border border-gray-300 rounded-sm py-2 hover:bg-sky-400">
         CLAIM YOUR FREE TRAIL TODAY
       </button>
 
-      <div className="flex bg-gray-400">
-        {partnerLogo.map((logo) => {
-          return (
-            <div className="w-1/2 mx-2">
-              <img src={logo} alt="partnerlogo" />
-            </div>
-          );
-        })}
+      <div className="grid grid-cols-3 items-center w-full">
+        <div>
+          <img className="mx-auto w-2/3" src={camtech} alt="" />
+        </div>
+        <div>
+          <img className="mx-auto w-1/4" src={techStartup} alt="" />
+        </div>
+        <div>
+          <img className="mx-auto w-1/2" src={nicc} alt="" />
+        </div>
+        <div>
+          <img className="mx-auto w-1/2" src={twd} alt="" />
+        </div>
+        <div>
+          <img className="mx-auto w-1/2" src={ideathon} alt="" />
+        </div>
       </div>
     </div>
   );
